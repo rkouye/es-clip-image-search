b38eab7ab7cac43b5102da00250849ca9228c894
rkouye <romaric.kouye@opendatasoft.com>
2021-04-08 12:32:12 +0200

    Refactor modules

--- scripts/commands.py
+++ scripts/commands.py
@@ -1,4 +1,5 @@
 import click
+from scripts.indexing import read_photos
 
 @click.group()
 def cli():
@@ -8,8 +9,7 @@ def cli():
 @click.argument('ids_filename', type=click.Path(exists=True))
 @click.argument('features_filename', type=click.Path(exists=True))
 def index_precomputed(ids_filename, features_filename):
-    click.echo(click.format_filename(ids_filename))
-    click.echo(click.format_filename(features_filename))
+    read_photos(ids_filename, features_filename)
 
 if __name__ == '__main__':
     cli()
--- scripts/indexing.py
+++ scripts/indexing.py
@@ -1,2 +1,9 @@
-def read_photos():
-    pass
+import pandas as pd
+import numpy as np
+
+def read_photos(ids_filename: str, features_filename: str):
+    print('Loading files...')
+    ids = pd.read_csv(ids_filename)
+    features = np.load(features_filename)
+    print('Files loaded.')
+    print(features.shape)
